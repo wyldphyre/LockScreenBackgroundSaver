16f034fcf664e1f4d3df5daa34ab95a1210e0ff0
Craig Reynolds <craigr@kestral.com.au>
2017-03-02 07:51:26 +0800

    Runs and handles basic argument detection

---
+++ LockScreenBackgroundSaver/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockScreenBackgroundSaver
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var MonitorForNewImages = false;
+      var OutputFolder = "";
+
+      foreach (var argument in args)
+      {
+        if (argument == "-monitor")
+        {
+          MonitorForNewImages = true;
+        }
+        else if (argument.StartsWith("-output:"))
+        {
+          OutputFolder = argument.Split(':').Last();
+        }
+      }
+
+      // TODO: Validate that OutputFolder exists
+    }
+  }
+}
