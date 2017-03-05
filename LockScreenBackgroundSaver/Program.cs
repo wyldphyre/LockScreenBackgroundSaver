bab44f6ea3b8e15a42dedb5293da7bb1d2f174bf
Craig Reynolds <wyldphyre@gmail.com>
2017-03-05 15:09:24 +0800

    Add the monitoring loop mechanism

--- LockScreenBackgroundSaver/Program.cs
+++ LockScreenBackgroundSaver/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Security.Cryptography;
+using System.Threading;
 
 namespace LockScreenBackgroundSaver
 {
@@ -35,29 +36,39 @@ namespace LockScreenBackgroundSaver
       // TODO: Validate that OutputFolder exists
 
       const int MinimumBackgroundSize = 1024 * 100;
+      //var MonitorInterval = TimeSpan.FromMinutes(10);
+      var MonitorInterval = TimeSpan.FromSeconds(10);
       var AssetPath = string.Format(@"C:\Users\{0}\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets", Environment.UserName);
-      var AssetDirectoryInfo = new DirectoryInfo(AssetPath);
-      var AssetFiles = AssetDirectoryInfo.GetFiles();
-      var PotentialBackgroundFiles = AssetFiles.Where(File => File.Length > MinimumBackgroundSize).ToArray();
-      var AssetHashFilePathDictionary = new Dictionary<string, string>();
 
-      var HashAlgorithm = SHA256Managed.Create();
-
-      foreach (var File in PotentialBackgroundFiles)
+      do
       {
-        using (var FileStream = new FileStream(File.FullName, FileMode.Open))
+        var AssetDirectoryInfo = new DirectoryInfo(AssetPath);
+        var AssetFiles = AssetDirectoryInfo.GetFiles();
+        var PotentialBackgroundFiles = AssetFiles.Where(File => File.Length > MinimumBackgroundSize).ToArray();
+        var AssetHashFilePathDictionary = new Dictionary<string, string>();
+
+        var HashAlgorithm = SHA256Managed.Create();
+
+        foreach (var File in PotentialBackgroundFiles)
         {
-          var HashReturnValue = HashAlgorithm.ComputeHash(FileStream);
+          using (var FileStream = new FileStream(File.FullName, FileMode.Open))
+          {
+            var HashReturnValue = HashAlgorithm.ComputeHash(FileStream);
 
-          var sb = new StringBuilder();
-          for (int i = 0; i < HashReturnValue.Length; i++)
-            sb.Append(BitConverter.ToString(HashReturnValue));
+            var sb = new StringBuilder();
+            for (int i = 0; i < HashReturnValue.Length; i++)
+              sb.Append(BitConverter.ToString(HashReturnValue));
 
-          var Hash = sb.ToString();
-          if (!AssetHashFilePathDictionary.ContainsKey(Hash))
-          AssetHashFilePathDictionary.Add(Hash, File.FullName);
+            var Hash = sb.ToString();
+            if (!AssetHashFilePathDictionary.ContainsKey(Hash))
+              AssetHashFilePathDictionary.Add(Hash, File.FullName);
+          }
         }
+
+        if (MonitorForNewImages)
+          Thread.Sleep(MonitorInterval);
       }
+      while (MonitorForNewImages);
     }
   }
 }
